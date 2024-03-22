974cca286200ce287cd33087db285cd41c1fde13
Kaua vinicius <santoskaua927@gmail.com>
2024-03-22 18:02:18 -0300

    22/03

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 module.exports = function (){
     return mysql.createConnection({
         host: 'localhost',
-        user: 'ifms',
-        password: 'ifms2024!',
+        user: 'root',
+        password: 'ifms',
         database: 'portal_noticias'
     });
 }
