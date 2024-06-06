8ab3a6f42cc16c69a03c2b92fdd7a8a7fa386c9c
Kaua vinicius <santoskaua927@gmail.com>
2024-06-06 18:38:13 -0300

    aula06/06

--- config/server.js
+++ config/server.js
@@ -6,6 +6,8 @@ var app = express();
 app.set('view engine','ejs'); //informa ao express que serpa usado ejs como engrenagem das views
 app.set('views','./app/views'); // indica o caminho das viwes
 
+app.use(express.static("./app/visual/"));
+
 consing().include("app/routes").into(app);
 
 module.exports = app;
